8199f6285076126f6c7dcaf838c337ac85d0fdfa
Usirusen Ikon <lexicon048@gmail.com>
2023-03-23 00:13:46 +0100

    Update logo button routing

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,55 @@
+import { Link, useLocation, useParams } from 'react-router-dom';
 import './Navbar.css';
 import '../../App.css';
 import Sidenav from './Sidenav/Sidenav';
+import projectData from '../../data/projectData';
 
 function Navbar() {
+  const location = useLocation();
+  const { id } = useParams();
+  const idx = projectData.find((project) => project.id === Number(id));
+  console.log(idx);
+
+  let content;
+
+  if (location.pathname === '/') {
+    content = (
+      <Link className="logo flex" to="/">
+        <i className="logo-text">Ikon`s</i>
+        <img
+          className="logo-img"
+          src="https://www.pngfind.com/pngs/m/168-1688149_web-clipart-cobweb-simple-spider-web-clipart-hd.png"
+          alt="web"
+        />
+      </Link>
+    );
+  } else if (location.pathname === '/projects') {
+    content = (
+      <Link className="logo flex" to="/">
+        <i className="logo-text">Ikon`s</i>
+        <img
+          className="logo-img"
+          src="https://www.pngfind.com/pngs/m/168-1688149_web-clipart-cobweb-simple-spider-web-clipart-hd.png"
+          alt="web"
+        />
+      </Link>
+    );
+  } else if (location.pathname === `/projects/${idx.id}`) {
+    content = (
+      <Link className="logo flex" to="/">
+        <i className="logo-text">Ikon`s</i>
+        <img
+          className="logo-img"
+          src="https://www.pngfind.com/pngs/m/168-1688149_web-clipart-cobweb-simple-spider-web-clipart-hd.png"
+          alt="web"
+        />
+      </Link>
+    );
+  }
+
   return (
     <div className="header flex animate">
-      <div className="header-headline flex">
-        <a href="#home" className="logo flex">
-            <i className="logo-text">Ikon`s</i>
-            <img className="logo-img" src="https://www.pngfind.com/pngs/m/168-1688149_web-clipart-cobweb-simple-spider-web-clipart-hd.png" alt="web" />
-        </a>
-      </div>
+      <div className="header-headline flex">{content}</div>
       <Sidenav />
     </div>
   );
--- src/components/Navbar/Sidenav/Sidenav.jsx
+++ src/components/Navbar/Sidenav/Sidenav.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react';
+import { Link, useLocation, useParams } from 'react-router-dom';
 import useTheme from '../../../hooks/useTheme';
 import navData from '../../../data/NavData';
 import Button from '../../../reusables/Button/Button';
 import styles from './Sidenav.module.css';
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+import HomeIcon from '@mui/icons-material/Home';
 import SocialIcons from '../../../reusables/Socialicons/SocialIcons';
 import { WbSunny, DarkMode } from '@mui/icons-material';
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
+import projectData from '../../../data/projectData';
 
 function Sidenav() {
   const [open, setopen] = useState(false);
@@ -15,6 +19,141 @@ function Sidenav() {
   };
 
   const [theme, setTheme] = useTheme();
+
+  const location = useLocation();
+  // const content = location.pathname === '/' ? (
+  //   <ul className={`flex ${styles.nav_list}`}>
+  //     {navData.map((item) => (
+  //       <Link
+  //         key={item.id}
+  //         className={`flex ${styles.nav_item}  ${
+  //           open ? styles.side_open : styles.side_close
+  //         }`}
+  //         to={item.link}
+  //         reloadDocument={item.link === '/' ? false : true}
+  //       >
+  //         <span
+  //           className={`flex animate ${
+  //             open ? styles.icon_open : styles.icon_close
+  //           }`}
+  //         >
+  //           {item.icon}
+  //         </span>
+  //         <span onClick={onClickHandler} className={`flex ${styles.nav_text}`}>
+  //           {item.text}
+  //         </span>
+  //       </Link>
+  //     ))}
+  //     <div
+  //       className={`flex ${styles.nav_icons} ${
+  //         open ? styles.see : styles.hide
+  //       }`}
+  //     >
+  //       <SocialIcons />
+  //     </div>
+  //   </ul>
+  // ) : (
+  //   <Link
+  //     className={`flex ${styles.nav_item}  ${
+  //       open ? styles.side_open : styles.side_close
+  //     }`}
+  //     to={'/'}
+  //   >
+  //     <span
+  //       className={`flex animate ${
+  //         open ? styles.icon_open : styles.icon_close
+  //       }`}
+  //     >
+  //       <HomeIcon />
+  //     </span>
+  //     <span onClick={onClickHandler} className={`flex ${styles.nav_text}`}>
+  //       Home
+  //     </span>
+  //   </Link>
+  // );
+  const { id } = useParams();
+  const idx = projectData.find((project) => project.id === Number(id));
+  console.log(idx);
+
+  let content;
+
+  if (location.pathname === '/') {
+    content = (
+      <ul className={`flex ${styles.nav_list}`}>
+        {navData.map((item) => (
+          <Link
+            key={item.id}
+            className={`flex ${styles.nav_item}  ${
+              open ? styles.side_open : styles.side_close
+            }`}
+            to={item.link}
+            reloadDocument={item.link === '/' ? false : true}
+          >
+            <span
+              className={`flex animate ${
+                open ? styles.icon_open : styles.icon_close
+              }`}
+            >
+              {item.icon}
+            </span>
+            <span
+              onClick={onClickHandler}
+              className={`flex ${styles.nav_text}`}
+            >
+              {item.text}
+            </span>
+          </Link>
+        ))}
+        <div
+          className={`flex ${styles.nav_icons} ${
+            open ? styles.see : styles.hide
+          }`}
+        >
+          <SocialIcons />
+        </div>
+      </ul>
+    );
+  } else if (location.pathname === '/projects') {
+    content = (
+      <Link
+        className={`flex ${styles.nav_item}  ${
+          open ? styles.side_open : styles.side_close
+        }`}
+        to={'/'}
+      >
+        <span
+          className={`flex animate ${
+            open ? styles.icon_open : styles.icon_close
+          }`}
+        >
+          <HomeIcon />
+        </span>
+        <span onClick={onClickHandler} className={`flex ${styles.nav_text}`}>
+          Home
+        </span>
+      </Link>
+    );
+  } else if(location.pathname === `/projects/${idx.id}`) {
+    content = (
+      <Link
+        className={`flex ${styles.nav_item}  ${
+          open ? styles.side_open : styles.side_close
+        }`}
+        to="/projects"
+      >
+        <span
+          className={`flex animate ${
+            open ? styles.icon_open : styles.icon_close
+          }`}
+        >
+          <AutoStoriesIcon />,
+        </span>
+        <span onClick={onClickHandler} className={`flex ${styles.nav_text}`}>
+          Projects
+        </span>
+      </Link>
+    );
+  }
 
   return (
     <div className={`animate ${styles.Sidenav}`}>
@@ -28,38 +167,7 @@ function Sidenav() {
           open ? styles.open : styles.close
         }`}
       >
-        <ul className={`flex ${styles.nav_list}`}>
-          {navData.map((item) => (
-            <a
-              key={item.id}
-              className={`flex ${styles.nav_item}  ${
-                open ? styles.side_open : styles.side_close
-              }`}
-              href={item.link}
-            >
-              <span
-                className={`flex animate ${
-                  open ? styles.icon_open : styles.icon_close
-                }`}
-              >
-                {item.icon}
-              </span>
-              <span
-                onClick={onClickHandler}
-                className={`flex ${styles.nav_text}`}
-              >
-                {item.text}
-              </span>
-            </a>
-          ))}
-          <div
-            className={`flex ${styles.nav_icons} ${
-              open ? styles.see : styles.hide
-            }`}
-          >
-            <SocialIcons />
-          </div>
-        </ul>
+        {content}
         <div
           className={`flex animate ${styles.theme}  ${
             open ? styles.theme_open : styles.theme_close
