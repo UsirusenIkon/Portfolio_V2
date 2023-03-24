4ba2b8e5fe7ecd4e51b2d8b41ceee3eb1fbb2a35
Usirusen Ikon <lexicon048@gmail.com>
2023-03-24 22:18:01 +0100

    Install react icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
+import { Outlet } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import HomePage from './components/Homepage/HomePage';
-import AboutPage from './components/Aboutpage/AboutPage';
-import SkillsPage from './components/Skillspage/SkillsPage';
-import ProjectsPage from './components/Projects/ProjectsPage';
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
     <div className="App animate">
       <Navbar />
-      <HomePage />
-      <AboutPage />
-      <SkillsPage />
-      <ProjectsPage />
+      <Outlet/>
     </div>
   );
 }
