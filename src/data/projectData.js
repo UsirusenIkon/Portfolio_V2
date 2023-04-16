f7fa1bacdaac1f9c7fff198d768ab6a6adac6759
Usirusen Ikon <lexicon048@gmail.com>
2023-04-16 19:41:58 +0100

    Add skill-icons object

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,6 +1,7 @@
 import todo from "../assets/todolist.svg";
 import leaderboard from "../assets/leaderboard.svg";
 import allblack  from "../assets/allblack.svg";
+import currencyApp from "../assets/currencyApp.svg";
 
 const projectData = [
   {
@@ -20,14 +21,12 @@ const projectData = [
     },
 
     description: {
-      summary: 'A website were clients can register for a basketball camp.',
-
       more: "This is a website for a basketball summer camp where clients can register and have full details about the basketball program. The website is fully responsive across devices and is built with HTML, CSS and JAVASCRIPT",
     },
 
     technologies: {
-      html: 'html',
-      css: 'css',
+      html: 'HTML',
+      css: 'CSS',
       js: 'javaScript',
     },
 
@@ -53,14 +52,12 @@ const projectData = [
     },
 
     description: {
-      summary: 'This project is a simple HTML list of To-Do tasks',
-
       more: "This project is a simple HTML list of To-Do tasks. It is built with HTML, CSS, javascript, and webpack. A user can add and remove to-do tasks and edit the task.",
     },
 
     technologies: {
-      html: 'html',
-      css: 'css',
+      html: 'HTML',
+      css: 'CSS',
       js: 'javaScript',
       webpack: 'webpack',
     },
@@ -87,14 +84,12 @@ const projectData = [
     },
 
     description: {
-      summary: 'This project is a leaderboard, here a user can add his name and score to the list',
-
-      more: "In this activity we set up a JavaScript project for the Leaderboard list app, using webpack and ES6 features, notably modules. Here a user inputs his name and score and it displays in the list",
+      more: "This project is a leaderboard, here a user can add his name and score to the list. In this activity we set up a JavaScript project for the Leaderboard list app, using webpack and ES6 features, notably modules. Here a user inputs his name and score and it displays in the list",
     },
 
     technologies: {
-      html: 'html',
-      css: 'css',
+      html: 'HTML',
+      css: 'CSS',
       js: 'javaScript',
       webpack: 'webpack',
     },
@@ -104,6 +99,39 @@ const projectData = [
       sources: 'https://github.com/UsirusenIkon/Leaderboard',
     },
   },
+  {
+    id: 3,
+
+    featuredImage: {
+      url: currencyApp,
+      alt: 'Currency-metrics-web-app',
+    },
+
+    title: 'Currency-metrics-web-app',
+
+    details: {
+      customer: 'Microverse',
+      job: 'Front-end',
+      year: 2022,
+    },
+
+    description: {
+      more: "Currency-metrics-web-app is an app that provides detailed information on currencies and exchange rates to update users on the latest exchange rates. Currency exchange metrics is built with React and redux to provide the state management, countries API to fetch the data.",
+    },
+
+    technologies: {
+      html: 'HTML',
+      css: 'CSS',
+      js: 'React',
+      webpack: 'webpack',
+    },
+
+    link: {
+      seeLive: 'https://relaxed-kleicha-1258c4.netlify.app/',
+      sources: 'https://github.com/UsirusenIkon/Currency-metrics-web-app',
+    },
+  },
+  
 ];
 
 export default projectData;
