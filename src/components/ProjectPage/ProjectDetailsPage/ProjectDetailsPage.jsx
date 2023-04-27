ee706fb70449e9012d7c486fbd52b63301665f7d
Usirusen Ikon <lexicon048@gmail.com>
2023-04-27 23:05:51 +0100

    Style update

--- src/components/ProjectPage/ProjectDetailsPage/ProjectDetailsPage.jsx
+++ src/components/ProjectPage/ProjectDetailsPage/ProjectDetailsPage.jsx
@@ -4,8 +4,7 @@ import { ArrowBackRounded, ArrowForwardRounded } from '@mui/icons-material';
 import styles from './Projectdetails.module.css';
 import Button from '../../../reusables/Button/Button';
 import Container from '../../../reusables/Container/Container';
-import { FaBeer } from "react-icons/fa";
-import { SiBattledotnet } from "react-icons/si";
+import { SiBattledotnet } from 'react-icons/si';
 import { Link } from 'react-router-dom';
 import './Projectdetails.module.css';
 
@@ -15,11 +14,11 @@ function ProjectsDetailsPage({ payload }) {
       <div className={`flex ${styles.project_cards}`}>
         <h1>{payload.title}</h1>
         <div className={`flex ${styles.link}`}>
-          <Link to={payload.link.seeLive} target="_blank" className={`btn ${styles.btn_link}`}>
-            See live
+          <Link to={payload.link.seeLive} target="_blank">
+            <Button text="See live" />
           </Link>
-          <Link to={payload.link.sources} target="_blank" className={`btn ${styles.btn_link}`}>
-            See Source
+          <Link to={payload.link.sources} target="_blank">
+            <Button text="See source" />
           </Link>
         </div>
         <div className="cards-image flex">
@@ -34,9 +33,9 @@ function ProjectsDetailsPage({ payload }) {
             <p className="topic">
               {payload.details.customer}
               <span className={`${styles.dott}`}>
-                <SiBattledotnet className={styles.dot}/>
+                <SiBattledotnet className={styles.dot} />
                 {payload.details.job}
-                <SiBattledotnet className={styles.dot}/>
+                <SiBattledotnet className={styles.dot} />
                 {payload.details.year}
               </span>
             </p>
@@ -47,7 +46,9 @@ function ProjectsDetailsPage({ payload }) {
             <li className={styles.backdrop}>{payload.technologies.js}</li>
             <li className={styles.backdrop}>{payload.technologies.ruby}</li>
           </ul>
-          <p className={`flex ${styles.description}`}>{payload.description.more}</p>
+          <p className={`flex ${styles.description}`}>
+            {payload.description.more}
+          </p>
         </div>
       </div>
     </Container>
