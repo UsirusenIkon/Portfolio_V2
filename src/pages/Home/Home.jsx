e00f5e735aa5b39f6318d5109d61c21e5e2448d9
Usirusen Ikon <lexicon048@gmail.com>
2023-04-28 06:52:30 +0100

    Resolve Import

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import HomePage from '../../components/Homepage/HomePage';
 import AboutPage from '../../components/Aboutpage/AboutPage';
 import SkillsPage from '../../components/Skillspage/SkillsPage';
-import ProjectsIndex from '../../components/ProjectPage/ProjectsIndex/ProjectsIndex';
+import ProjectsIndex from '../../components/ProjectPage/ProjectsPage/ProjectsIndex';
 import ContactPage from '../../components/ContactPage/ContactPage';
 
 function Home() {
